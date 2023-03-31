459bb3b209517651d0905c711fd59c43e3e38bdd
gitfuego <firaskhansa3@gmail.com>
2023-03-31 03:14:52 -0400

    added sass, fixed up landing page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 export default function() {
+  function handleClick() {
+    window.alert('yurrr');
+  }
   return (
     <div className="background">
       {/* <div className="logo">
@@ -9,9 +12,18 @@ export default function() {
           <img className="logo" src="/mykoWhite.svg" />
         </div>
         <div className="topSub">
-          <button>LOGIN</button>
+          <button onClick={handleClick}>LOGIN</button>
         </div>
       </header>
+      <section className="splash">
+        <div className="landing-text">
+          <div className="main">Where artists and their top listeners hangout.</div>
+          <div className="sub">Connect with Spotify and see which communities you qualify for!</div>
+        </div>
+        <div className="landing-img-container">
+          <img id="landing-phone" src='/landingPhone.svg' />
+        </div>
+      </section>
     </div>
   );
 };
