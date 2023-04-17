7d1b0a0d8fa5ceb832f3679410f86bf0bf8b8eb3
gitfuego <firaskhansa3@gmail.com>
2023-04-16 23:54:46 -0400

    added api endpoints for signup/login and messages

---
+++ pages/api/messages/[artistID].js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // Process a POST request
+  } else {
+    // Handle any other HTTP method
+  }
+}
---
+++ pages/api/users/[userID].js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method === 'GET') {
+    // Process a GET request
+  } else {
+    // Handle any other HTTP method
+  }
+}
